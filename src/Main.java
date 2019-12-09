029778f24db7cb54ac7ee4850dc4c146d135a84b
realeigenvalue <57877219+realeigenvalue@users.noreply.github.com>
2019-12-08 23:10:00 -0600

    Add Main class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import mvc.ClientDesktopApp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClientDesktopApp desktopApp = new ClientDesktopApp();
+	}
+}
